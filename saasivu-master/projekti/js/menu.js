ab124340b1c54cfb72533fa77393a1aae4f373f3
markuaha <62019000+markuaha@users.noreply.github.com>
2020-03-11 16:44:04 +0200

    Add files via upload

---
+++ saasivu-master/projekti/js/menu.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.getElementById('logo').addEventListener('click', function () {
+    // scroll etusivulle
+    window.scrollTo(0, 0);
+});
+
+document.getElementById('box1').addEventListener('click', function () {
+    //scroll sää
+    window.scrollTo(0, 85);
+});
+
+document.getElementById('box2').addEventListener('click', function () {
+    //scroll videoon
+    window.scrollTo(0, 0);
+});
+
+document.getElementById('box3').addEventListener('click', function () {
+    //scroll yhteystietoihin/kuviin
+    window.scrollTo(0, 0);
+});
